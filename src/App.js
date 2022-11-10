f5a1a3ef878a3099021c02c7f2324d4e14b0db22
CGJI58 <leemsk22@naver.com>
2022-11-10 21:54:06 +0900

    ~#2.3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${({ bgColor }) => bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50%;
+`;
+
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
+`;
+
 function App() {
-  return null;
+  return (
+    <Father as={"main"}>
+      <Box bgColor="black" />
+      <Circle bgColor="teal" />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+    </Father>
+  );
 }
 
 export default App;
