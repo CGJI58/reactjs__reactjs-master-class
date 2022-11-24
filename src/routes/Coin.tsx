ffc8cf172aa67b30b9e3582e0f44bbfea6cd6567
CGJI58 <leemsk22@naver.com>
2022-11-24 16:48:30 +0900

    ~#5.6

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -26,31 +26,88 @@ const Header = styled.header`
   align-items: center;
 `;
 
-interface RouteParams {
+interface IRouteParams {
   coinId: string;
 }
 
-interface RouteState {
+interface IRouteState {
   name: string;
+}
+
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
 }
 
 function Coin() {
   const [loading, setLoading] = useState(true);
-  const { coinId } = useParams<RouteParams>();
-  const { state } = useLocation<RouteState>();
-  const [info, setInfo] = useState({});
-  const [price, setPrice] = useState({});
+  const { coinId } = useParams<IRouteParams>();
+  const { state } = useLocation<IRouteState>();
+  const [info, setInfo] = useState<IInfoData>();
+  const [price, setPrice] = useState<IPriceData>();
 
   useEffect(() => {
     (async () => {
-      const infoData = await axios(
+      const infoData: IInfoData = await axios(
         `https://api.coinpaprika.com/v1/coins/${coinId}`
       );
-      const priceData = await axios(
+      const priceData: IPriceData = await axios(
         `https://api.coinpaprika.com/v1/tickers/${coinId}`
       );
       setInfo(infoData);
       setPrice(priceData);
+      console.log(priceData);
       setLoading(false);
     })();
   }, []);
