1c5ccf19979d437127b7af2cf0c027d5faf3f223
CGJI58 <leemsk22@naver.com>
2022-12-02 17:14:31 +0900

    ~#5.12

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 const BASE_URL = "https://api.coinpaprika.com/v1";
+const CHART_URL = "https://ohlcv-api.nomadcoders.workers.dev";
 
 export async function fetchCoins() {
   const json = await (await fetch(`${BASE_URL}/coins`)).json();
@@ -12,5 +13,10 @@ export async function fetchCoinInfo(coinId: string) {
 
 export async function fetchCoinTicker(coinId: string) {
   const json = await (await fetch(`${BASE_URL}/tickers/${coinId}`)).json();
+  return json;
+}
+
+export async function fetchCoinHistory(coinId: string) {
+  const json = await (await fetch(`${CHART_URL}?coinId=${coinId}`)).json();
   return json;
 }
